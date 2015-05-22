64c5b248cb48074aeb933b12ccd42017093f0dc5
Reid Vandewiele <reid@puppetlabs.com>
2015-05-22 10:47:23 -0700

    Add server service

--- vendor/torque/manifests/server.pp
+++ vendor/torque/manifests/server.pp
@@ -8,12 +8,19 @@ class torque::server (
     ensure => installed,
   }
 
+  service { 'pbs_server':
+    ensure  => running,
+    enable  => true,
+    require => Package['torque-server'],
+  }
+
   @@torque::config::server_name { $::fqdn: }
 
   $nodefile = "${torque::torque_home}/server_priv/nodes"
 
   concat { $nodefile:
     ensure => present,
+    notify => Service['torque-server'],
   }
 
   Concat::Fragment <<| tag == $torque::cluster_tag and target == $nodefile |>>
