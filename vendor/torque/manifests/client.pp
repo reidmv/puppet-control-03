9b45721bfbed6b1ec3fa9bd21d87b396be4e5fc7
Reid Vandewiele <reid@puppetlabs.com>
2015-05-22 10:08:29 -0700

    add service

--- vendor/torque/manifests/client.pp
+++ vendor/torque/manifests/client.pp
@@ -7,12 +7,21 @@ class torque::client (
     ensure => installed,
   }
 
-  Torque::Config::Server_name <<| tag == $torque::cluster_tag |>>
+  service { 'pbs_mom':
+    ensure    => running,
+    enable    => true,
+    require   => Package['torque-client'],
+  }
+
+  Torque::Config::Server_name <<| tag == $torque::cluster_tag |>> {
+    notify => Service['pbs_mom'],
+  }
 
   @@concat::fragment { "torque_node_${::clientcert}":
     target  => "${torque::torque_home}/server_priv/nodes",
     content => "${::fqdn}\n",
     tag     => $torque::cluster_tag,
+    notify  => Service['pbs_mom'],
   }
 
 }
